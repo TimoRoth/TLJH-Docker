12b77d66736c536f68ab0c3e15ab7c082bedafa0
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2025-02-01 23:39:19 +0100

    Use local config dir

--- update.sh
+++ update.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 set -xe
+
+if ! [[ -d config ]]; then
+	mkdir config
+	cp tljh/config.yaml config
+	mkdir -p config/templates/extra-assets
+	mkdir -p config/jupyterhub_config.d
+fi
+
 docker compose pull
 docker compose build --pull
 docker compose up -d
